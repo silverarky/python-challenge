d12ae43a4800cfc9e804443df1f8ac2aa2a833bd
silverarky <543501581@qq.com>
2017-05-12 11:00:45 +0800

    add maketrans

--- cha_2.py
+++ cha_2.py
@@ -24,3 +24,19 @@ raw="g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle 
 str_c(raw)
 #join使用不了
 #for不能改变原数据
+
+#使用maketrans()函数
+raw="g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+def chan(x):
+    fo=''
+    for i in x:
+        i=chr(i)
+        fo=fo+i
+    return fo
+input=list(range(97,123))
+output=list(range(99,123))
+output.extend([97,98])
+input=chan(input)
+output=chan(output)
+trantab=str.maketrans(input,output)
+print (raw.translate(trantab))
