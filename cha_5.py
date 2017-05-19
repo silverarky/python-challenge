cee0ad7c9aa8e65ebcb30b6f7f0a0d7f0115e6c7
silverarky <543501581@qq.com>
2017-05-19 21:56:53 +0800

    cha_5

---
+++ cha_5.py
@@ -0,0 +1,22 @@
+#-*- coding:utf-8 -*- - 
+#获取源码并保存
+import urllib.request as urllib
+import pickle
+import pprint
+html=urllib.urlopen("http://www.pythonchallenge.com/pc/def/banner.p")
+res=pickle.Unpickler(html).load()
+pprint.pprint(res)
+
+for i in res:
+    sp=""
+    for j in i:
+        for k in range(j[1]):
+            sp+=j[0]
+    print(sp)
+
+#--------------------
+#循环部分优化
+for i in res:
+
+#join需要定义sep,不然会有NameError
+        
